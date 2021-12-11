add95b15afda1234284ae640dd85352bcb77f513
Gabriel Angel Gil Fernandez <gabrielangelgf@hotmail.com>
2021-12-11 01:05:28 -0500

    Fin sección 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
+import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: Grafica1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-        ]
-    },
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },    
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: '**', component: NotPageFoundComponent }
 ];
 
 @NgModule({
     declarations: [],
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes),
+        PagesRoutingModule,
+        AuthRoutingModule
     ],
     exports: [RouterModule]
 })
